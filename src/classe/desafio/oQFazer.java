041bd9b76995b2eb7c897ccd78a9a6dcdcdf728a
vitot <victorfarian@gmail.com>
2023-05-01 21:41:29 -0300

    Mandando os arquivos

---
+++ src/classe/desafio/oQFazer.java
@@ -0,0 +1,19 @@
+package classe.desafio;
+
+public @interface oQFazer {
+//	3 classes
+//	1 pessoa,
+//		nome
+//		peso
+//		comer()
+//			recebe por parametro um objeto da classe comida
+//			peso pessoa + peso comida
+//mostrar peso antes e depois de comer
+//	2 comida,
+//		nome
+//		peso
+//
+//	3 jantar:
+//		main
+
+}
---
+++ src/fundamentos/tipos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos.tipos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Primeiro Programa!!!");
+	}
+
+}
