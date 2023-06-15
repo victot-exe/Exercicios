6b7c7b8aa5a9026f98b7b3c5237665d9bd64f0ae
vitot <victorfarian@gmail.com>
2023-06-14 21:56:56 -0300

    Aprofundando Streams (Map e filter)

---
+++ src/streams/Aluno.java
@@ -0,0 +1,14 @@
+package src.streams;
+
+public class Aluno {
+	
+	final String nome;
+	final double nota;
+	
+	public Aluno(String nome, double nota) {
+		this.nome = nome;
+		this.nota = nota;
+	}
+	
+	
+}
