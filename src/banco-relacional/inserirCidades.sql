6d33495a48588f9b021d937b34fabd87551b86ce
vitot <victorfarian@gmail.com>
2023-08-21 23:18:15 -0300

    Mais estudos sobre BDs

---
+++ src/banco-relacional/inserirCidades.sql
@@ -0,0 +1,23 @@
+select * from estados where id = 28
+
+insert into cidades(nome, area, estado_id)
+values('Campinas', 795, 28)
+
+insert into cidades(nome, area, estado_id)
+values('Niterói', 133.9, 22)
+
+insert into cidades(nome, area, estado_id)
+values(
+    'Caruaru',
+    920.6,
+    (select id from estados where sigla = 'PE')
+)
+
+insert into cidades(nome, area, estado_id)
+values(
+    'Juazeiro do Norte',
+    248.2,
+    (select id from estados where sigla = 'CE')
+)
+
+select * from cidades
